b6f94469a1e2eb898a9003638d41cd00b248e0a4
PC-11 <mz2820031@gmail.com>
2025-10-22 12:18:12 +0300

    dcvszdkplkl,kl,dvrfr

--- app/src/main/java/com/example/mathexercise/MainActivity.java
+++ app/src/main/java/com/example/mathexercise/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mathexercise;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private int rightCount;
     private TextView wrong;
     private int wrongCount;
+    private TextView mark;
+    private int markCount;
 
     private TextView num1;
     private TextView num2;
@@ -23,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
     private Button check;
 
     private EditText answer;
-
+    private boolean s,t,c;
     private int r1, r2;
+    private int count;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,15 +49,24 @@ public class MainActivity extends AppCompatActivity {
         challenge = findViewById(R.id.chalngeId);
         check = findViewById(R.id.checkId);
         answer = findViewById(R.id.answerId);
+        mark = findViewById(R.id.markId);
+        markCount=0;
+        count=0;
+        s=false;
+        t=false;
+        c=false;
 
         single.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                r1 = (int) (Math.random() * 9) + 1;
-                r2 = (int) (Math.random() * 9) + 1;
-                num1.setText(r1+"");
-                num2.setText(r2+"");
-                answer.setText("");
+                if (count >= 0){
+                    r1 = (int) (Math.random() * 9) + 1;
+                    r2 = (int) (Math.random() * 9) + 1;
+                    num1.setText(r1 + "");
+                    num2.setText(r2 + "");
+                    answer.setText("");
+                    s = true;
+                }
             }
         });
 
@@ -59,22 +74,28 @@ public class MainActivity extends AppCompatActivity {
         till20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                r1 = (int) (Math.random() * 11) + 10;
-                r2 = (int) (Math.random() * 9) + 1;
-                num1.setText(r1 + "");
-                num2.setText(r2 + "");
-                answer.setText("");
+                if (count >= 10) {
+                    r1 = (int) (Math.random() * 11) + 10;
+                    r2 = (int) (Math.random() * 9) + 1;
+                    num1.setText(r1 + "");
+                    num2.setText(r2 + "");
+                    answer.setText("");
+                    t = true;
+                }
             }
         });
 
         challenge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                r1 = (int) (Math.random() * 100) + 1;
-                r2 = (int) (Math.random() * 100) + 1;
-                num1.setText(r1+"");
-                num2.setText(r2+"");
-                answer.setText("");
+                if (count >= 20) {
+                    r1 = (int) (Math.random() * 100) + 1;
+                    r2 = (int) (Math.random() * 100) + 1;
+                    num1.setText(r1 + "");
+                    num2.setText(r2 + "");
+                    answer.setText("");
+                    c = true;
+                }
             }
         });
 
@@ -96,12 +117,34 @@ public class MainActivity extends AppCompatActivity {
                     num1.setText("");
                     num2.setText("");
                     answer.setText("");
+                    if(s){
+                        markCount+=2;
+                        count++;}
+                    if(t){
+                        markCount+=5;
+                        count++;}
+                    if(c)
+                        markCount+=10;
+                    mark.setText("mark:"+markCount);
+                    s=false;
+                    t=false;
+                    c=false;
+
                 } else {
                     wrongCount++;
                     wrong.setText("wrong:"+wrongCount);
                     num1.setText("");
                     num2.setText("");
                     answer.setText("");
+                    if(s)
+                        count=0;
+                    if(t)
+                        count=10;
+                    if(c)
+                        count=20;
+                    s=false;
+                    t=false;
+                    c=false;
                 }
             }
         });
